699c296b69431272c443209bd466a7e2700fba3c
Clelton <cleltonjm_95@hotmail.com>
2025-06-03 14:44:27 -0300

    feat: create base ui component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from "./components/secondary-button/secondary-button.component";
 import { ItemCertifiedComponent } from "./components/item-certified/item-certified.component";
+import { BaseUiComponent } from "./components/base-ui/base-ui.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertifiedComponent],
+  imports: [
+    NavbarComponent, 
+    PrimaryButtonComponent, 
+    SecondaryButtonComponent, 
+    ItemCertifiedComponent, 
+    BaseUiComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
