d014696afb609c9ae5343c1bf3ee6d736aba4dec
Clelton <cleltonjm_95@hotmail.com>
2025-06-04 13:49:12 -0300

    feat: create certificate form page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { BaseUiComponent } from "./components/base-ui/base-ui.component";
 import { CertificadesComponent } from "./pages/certificades/certificades.component";
+import { CertificateFormComponent } from "./pages/certificate-form/certificate-form.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
     BaseUiComponent,
-    CertificadesComponent
+    CertificadesComponent,
+    CertificateFormComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/pages/certificades/certificades.component.ts
+++ src/app/pages/certificades/certificades.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../components/secondary-button/secondary-button.component";
+import { ItemCertifiedComponent } from "../../components/item-certified/item-certified.component";
 
 @Component({
   selector: 'app-certificades',
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, ItemCertifiedComponent],
   templateUrl: './certificades.component.html',
   styleUrl: './certificades.component.css'
 })
