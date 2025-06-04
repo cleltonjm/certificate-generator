5d905e660a8966e3ecc5d7e65ae2cbb020a04477
Clelton <cleltonjm_95@hotmail.com>
2025-06-04 16:12:14 -0300

    feat: add certificate page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { BaseUiComponent } from "./components/base-ui/base-ui.component";
-import { CertificadesComponent } from "./pages/certificades/certificades.component";
-import { CertificateFormComponent } from "./pages/certificate-form/certificate-form.component";
+import { CertificateComponent } from "./pages/certificate/certificate.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
     BaseUiComponent,
-    CertificadesComponent,
-    CertificateFormComponent
+    CertificateComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
