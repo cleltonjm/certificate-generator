c19d92e3989d29a4cf43e2a26c09e8070e09d01c
Clelton <cleltonjm_95@hotmail.com>
2025-06-05 14:36:29 -0300

    feat: form validators

--- src/app/pages/certificate-form/certificate-form.component.ts
+++ src/app/pages/certificate-form/certificate-form.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../components/secondary-button/secondary-button.component";
 import { PrimaryButtonComponent } from "../../components/primary-button/primary-button.component";
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificate-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  imports: [SecondaryButtonComponent, PrimaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificate-form.component.html',
   styleUrl: './certificate-form.component.css'
 })
 export class CertificateFormComponent {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = [];
 
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched
+  }
 }
